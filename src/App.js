444311a4255e5e9861d4d9bdd652dd8b865c0cc8
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-02-21 19:19:47 -0800

    add component file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CategoryItem from './components/category-item/category-item.component';
 import './categories.styles.scss'
 
 function App() {
@@ -33,16 +34,8 @@ function App() {
 
   return (
     <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div key={id} className='category-container'>
-          <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-          }}/>
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
       ))}
     </div>
   )
