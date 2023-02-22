797bb4a0edb3ae80e8d6e616b287f37f747d6f6d
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-02-21 18:04:02 -0800

    initial skeleton structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    }, {
+      id: 2,
+      title: 'Jackets'
+    }, {
+      id: 3,
+      title: 'Sneakers'
+    }, {
+      id: 4,
+      title: 'Womens'
+    }, {
+      id: 5,
+      title: 'Mens'
+    }, 
+  ];
+
   return (
-    <div className="App">
-      <h1>Ecomm Site</h1>
+    <div className="categories-container">
+      {categories.map(category => (
+        <div className='category-container'>
+        <div className='background-image' />
+        <div className='category-body-conatiner'>
+          <h2>{category.title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
+
+        
